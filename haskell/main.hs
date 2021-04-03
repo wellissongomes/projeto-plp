634b6d89e47d14ecc80665b1fd68c7cbe9f2149a
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2021-04-03 13:19:56 -0300

    clear files

--- haskell/main.hs
+++ haskell/main.hs
@@ -9,20 +9,17 @@ import DB
 import Data.List.Split
 
 main = do
-  let f1 = Employee 1 "10220579440" "afawfawfawff Gomes" 20 "Caixa"
-  let d1 = Candy 1 "Sorvete de chocolate" "Sorvete de chocolate e calda morango" 15.96
-  let c1 = Customer 1 "10220579440" "Lucas Gomes" 20 "rua tal"
-  let compra = Purchase 1 1 1 5 [d1, d1, d1, d1, d1, d1]
-  
-  -- appendFile "./db/funcionario.txt" (toString f1 ++ "\n")
-  -- appendFile "./db/cliente.txt" (toString c1 ++ "\n")
-  -- appendFile "./db/doce.txt" (toString d1 ++ "\n")
-  -- appendFile "./db/compra.txt" (toString compra ++ "\n")
+  -- let f1 = Employee 1 "10120379940" "Wellisson Gomes" 20 "Caixa"
+  -- let d1 = Candy 1 "Sorvete de chocolate" "Sorvete de chocolate e calda morango" 15.96
+  -- let c1 = Customer 1 "10120379940" "Gomes Gomes" 20 "rua tal"
+  -- let compra = Purchase 1 1 1 5 [d1, d1, d1, d1, d1, d1]
 
-  DB.addToFile "./db/funcionario.txt" f1
-  DB.addToFile "./db/cliente.txt" c1
-  DB.addToFile "./db/doce.txt" d1
-  DB.addToFile "./db/compra.txt" compra
+  -- DB.addToFile "./db/funcionario.txt" f1
+  -- DB.addToFile "./db/cliente.txt" c1
+  -- DB.addToFile "./db/doce.txt" d1
+  -- DB.addToFile "./db/compra.txt" compra
 
-  dados <- DB.connect
-  print (DB.employees dados)
+  -- dados <- DB.connect
+  -- print (DB.employees dados)
+
+  DB.deleteAll
