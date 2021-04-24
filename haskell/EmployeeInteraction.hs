0ed0288ea6e3100a608fa437f2df0319274c8393
eric <eric.goncalves@ccc.ufcg.edu.br>
2021-04-23 21:36:59 -0300

    otimização cadastro de funcionário como cliente

--- haskell/EmployeeInteraction.hs
+++ haskell/EmployeeInteraction.hs
@@ -4,9 +4,12 @@ import DB
 import Customer
 import Utils
 
+import Employee
+
 registerCustomer :: DB -> Interaction -> Int -> IO ()
 registerCustomer db employeeInteraction employeeId = do
   let customers = DB.customers db
+  let employees = DB.employees db
 
   let customerId = (DB.currentIdCustomer db) + 1
 
@@ -14,6 +17,17 @@ registerCustomer db employeeInteraction employeeId = do
   if existsPerson customers ssn then do
     putStr "CPF já cadastrado.\n"
     registerCustomer db employeeInteraction employeeId
+  else if existsPerson employees ssn then do
+    let employee = getEntityById employees employeeId
+    let employeeSsn = Employee.ssn employee
+    let employeeName = Employee.name employee
+    let employeeAge = Employee.age employee
+    address <- input $ employeeName ++ ", informe seu endereço: "
+
+    let customer = (Customer customerId employeeSsn  employeeName employeeAge address)
+
+    saveCustomer db employeeInteraction employeeId customerId customer customers
+
   else do
     name <- input "Nome: "
     age <- input "Idade: "
@@ -21,12 +35,15 @@ registerCustomer db employeeInteraction employeeId = do
 
     let customer = (Customer customerId ssn name (read age) address)
 
-    DB.entityToFile customer "cliente.txt" "custId.txt"
-    let newDB = db {DB.customers = customers ++ [customer], DB.currentIdCustomer = customerId}
+    saveCustomer db employeeInteraction employeeId customerId customer customers
 
-    clear
-    putStr "Cliente cadastrado com sucesso!"
-    putStr $ show customer
-    waitThreeSeconds
-  
-    employeeInteraction newDB employeeId
+saveCustomer :: DB -> Interaction -> Int -> Int -> Customer -> [Customer] -> IO()
+saveCustomer db employeeInteraction employeeId customerId customer customers = do
+  DB.entityToFile customer "cliente.txt" "custId.txt"
+  let newDB = db {DB.customers = customers ++ [customer], DB.currentIdCustomer = customerId}
+
+  clear
+  putStr "Cliente cadastrado com sucesso!"
+  putStr $ show customer
+  waitThreeSeconds
+  employeeInteraction newDB employeeId
